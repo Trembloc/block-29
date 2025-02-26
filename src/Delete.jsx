25dc0916240c6cf29d869faebe2cb754f358803c
Trembloc <tremblay34487@gmail.com>
2025-02-26 12:09:43 -0500

    added basic forms layout

---
+++ src/Delete.jsx
@@ -0,0 +1,17 @@
+const DeleteForm = () => {
+  return (
+    <>
+      <h1>
+        Remove Puppy Here
+      </h1>
+      <form>
+        <label>
+          Id: <input />
+        </label>
+        <button>Delete</button>
+      </form>
+    </>
+  )
+}
+
+export default DeleteForm
---
+++ src/PuppyList.jsx
@@ -0,0 +1,13 @@
+const PuppyList = () => {
+  return (
+    <ul>
+      <li>this</li>
+      <li>will </li>
+      <li>use</li>
+      <li>Fetch</li>
+      <li>to</li>
+      <li>show the puppies API</li>
+    </ul>
+  )
+}
+export default PuppyList
